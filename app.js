5d0a2fd3f197b2eafc6ab44e2d587e303124ff47
MARCO <jcmm.1994@gmail.com>
2026-04-14 16:07:46 +0100

    base

---
+++ app.js
@@ -0,0 +1,11 @@
+const userListContainer = document.getElementById("user-list-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+
+console.log(userListContainer);
+console.log(loadMoreBtn);
+
+let skip = 0;
+const limit = 10;
+
+console.log("skip actual:", skip);
+console.log("limit actual:", limit);
